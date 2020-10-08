61ff30b19d55822e21108ffa79b7afd36ba19980
EgorTrif <72250150+EgorTrif@users.noreply.github.com>
2020-10-08 14:42:13 +0300

    Add files via upload

---
+++ 2/exercise2.js
@@ -0,0 +1,3 @@
+var str = "123№№22№33№44"
+  
+console.log (("123№№223№3№44".match(new RegExp("№", "g"))).length);
---
+++ 2/exercise5.js
@@ -0,0 +1,8 @@
+var numbers = [6,9,2,5];
+var sum=0;
+
+function avarege(numbers) {
+for(var i in arr) { sum += numbers[i]; }
+result = sum / numbers.length;
+} 
+console.log(result)
